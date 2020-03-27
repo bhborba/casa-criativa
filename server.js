6484ed3cdfa5ee44cb888a095d986fcdc52b4274
bhborba <brunohenriqueborba@gmail.com>
2020-03-27 15:19:53 -0300

    feito conexao com sqlite e cadastro de ideias

--- server.js
+++ server.js
@@ -1,38 +1,11 @@
 const express = require("express");
 const server = express();
 
-const ideas = [
-    {
-        img: "https://image.flaticon.com/icons/svg/2729/2729007.svg",
-        title: "Cursos de programação",
-        category: "Estudo",
-        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde, qui accusantium. Temporibus saepe minima ex laudantium adipisci dolor ipsa ad nostrum voluptates molestiae fugiat corrupti et, consequatur officia vero accusamus!",
-        url: "google.com",
-    },
-    {
-        img: "https://image.flaticon.com/icons/svg/2729/2729005.svg",
-        title: "Exercícios",
-        category: "Saúde",
-        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde, qui accusantium. Temporibus saepe minima ex laudantium adipisci dolor ipsa ad nostrum voluptates molestiae fugiat corrupti et, consequatur officia vero accusamus!",
-        url: "google.com",
-    },
-    {
-        img: "https://image.flaticon.com/icons/svg/2729/2729027.svg",
-        title: "Meditação",
-        category: "Mentalidade",
-        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde, qui accusantium. Temporibus saepe minima ex laudantium adipisci dolor ipsa ad nostrum voluptates molestiae fugiat corrupti et, consequatur officia vero accusamus!",
-        url: "google.com",
-    },
-    {
-        img: "https://image.flaticon.com/icons/svg/2729/2729032.svg",
-        title: "Cursos de Karaoke",
-        category: "Diversão e Família",
-        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde, qui accusantium. Temporibus saepe minima ex laudantium adipisci dolor ipsa ad nostrum voluptates molestiae fugiat corrupti et, consequatur officia vero accusamus!",
-        url: "google.com",
-    }
-]
+const db = require("./db")
 
 server.use(express.static("public"));
+
+server.use(express.urlencoded({ extended: true}))
 
 const nunjucks = require("nunjucks");
 nunjucks.configure("views", {
@@ -42,20 +15,65 @@ nunjucks.configure("views", {
 
 server.get('/', function(req,res) {
 
-    const reversedIdeas = [...ideas].reverse();
-    let lastIdeas =  []; 
-    for (let idea of reversedIdeas){
-        if(lastIdeas.length < 2){
-            lastIdeas.push(idea)
+    // Consultar dados na tabela
+    db.all(`SELECT * FROM ideas`, function(err, rows){
+        if (err) {
+            console.log(err)
+            return res.send("Erro no banco de dados")
         }
-    }
 
-    return res.render('index.html' ,{ ideas: lastIdeas});
+        const reversedIdeas = [...rows].reverse();
+        let lastIdeas =  []; 
+        for (let idea of reversedIdeas){
+            if(lastIdeas.length < 2){
+                lastIdeas.push(idea)
+            }
+        }
+    
+        return res.render('index.html' ,{ ideas: lastIdeas});
+    })
 })
 
+server.post('/', function(req,res) {
+
+    const query = `
+        INSERT INTO ideas(
+            image,
+            title,
+            category,
+            description,
+            link
+        ) VALUES (?,?,?,?,?);
+    `;
+
+    const values = [
+        req.body.image,
+        req.body.title,
+        req.body.category,
+        req.body.description,
+        req.body.link
+    ]
+    db.run(query, values, function(err){
+        if (err) {
+            console.log(err)
+            return res.send("Erro no banco de dados")
+        }
+
+        return res.redirect("/ideias")
+    });
+})
+
+
 server.get('/ideias', function(req,res) {
-    const reversedIdeas = [...ideas].reverse();
-    return res.render('ideias.html', { ideas: reversedIdeas});
+    db.all(`SELECT * FROM ideas`, function(err, rows){
+        if (err) {
+            console.log(err)
+            return res.send("Erro no banco de dados")
+        }
+        const reversedIdeas = [...rows].reverse();
+        return res.render('ideias.html', { ideas: reversedIdeas});
+    })
+    
 })
 
 
